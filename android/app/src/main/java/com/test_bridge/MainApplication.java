adbcdea96b6f3c74fa038493010adf7d7e1bbd68
bodaghialib4 <bodaghialib4@gmail.com>
2019-10-10 12:17:37 +0330

    Register the ToastModule

--- android/app/src/main/java/com/test_bridge/MainApplication.java
+++ android/app/src/main/java/com/test_bridge/MainApplication.java
@@ -1,5 +1,6 @@
 package com.test_bridge;
 
+import com.test_bridge.CustomToastPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new CustomToastPackage());
           return packages;
         }
 
